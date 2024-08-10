29c5b672defc649c41807edce92516d6abd0f9a5
Muhammad Tayyab <786.mtayyab@gmail.com>
2024-08-10 11:02:06 +0500

    check

---
+++ app.js
@@ -0,0 +1,13 @@
+// ["bat","tap","cat","tab","pat"]
+// [['bat','tab'],['tap','pat'],['cat']]
+
+
+var arrUser = ["bat","tap","cat","tab","pat"];
+
+var combWord = [];
+
+for(i=0; i<arrUser.length;i++){
+    if(arrUser[i]===())
+    console.log(arrUser)
+}
+
